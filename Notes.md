f5829914ea4d67cd14cb02699aced21ede59c661
Pushpak-bhoite <bhoitepushpak6@gmail.com>
2026-05-28 19:55:18 +0530

    RAG: complete RAG pipeline using OpenAI lanchain

--- Notes.md
+++ Notes.md
@@ -1,2 +1,13 @@
+** Docs for LangChain Openai integration ** 
 for http request/response = https://docs.aiohttp.org/en/stable/
 structured output pydentic openai = https://developers.openai.com/api/docs/guides/structured-outputs
+pypdf document loader = https://reference.langchain.com/python/langchain-community/document_loaders/pdf/PyPDFLoader
+Splitter = https://docs.langchain.com/oss/python/integrations/splitters
+Embedding = https://docs.langchain.com/oss/python/integrations/embeddings/openai#installation
+LangChanin OpeAI Embedding = https://docs.langchain.com/oss/python/integrations/embeddings/openai#instantiation
+
+Options for Embedding (we can do free) - https://docs.langchain.com/oss/python/integrations/vectorstores#top-integrations
+
+quadrant vector store - https://docs.langchain.com/oss/python/integrations/vectorstores/qdrant
+
+-----------------
--- RAG/index.py
+++ RAG/index.py
@@ -1,7 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_qdrant import QdrantVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
- 
+from dotenv import load_dotenv
+
+load_dotenv()
 #1. Loading
 loader = PyPDFLoader(
     file_path = "./rag-health.pdf",
@@ -16,9 +19,9 @@ print(f"First page -> {docs[0]}")
 #2. Splitting
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)  # overlap - is every chunk takes some part of it's previous chunk(text) so it helps every chunk to keep context. 
 #It's kind of recap from previous chunk
-texts = text_splitter.split_documents(docs)
+chunks = text_splitter.split_documents(docs)
 
-#3. Embedding
+#3. Embedding Model 
 embeddings = OpenAIEmbeddings(
     model="text-embedding-3-large",
     # With the `text-embedding-3` class
@@ -26,3 +29,15 @@ embeddings = OpenAIEmbeddings(
     # of the embeddings you want returned.
     # dimensions=1024
 )
+
+#4. quadrant vector storage 
+qdrant = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embeddings=embeddings,
+    url="http://localhost:6333", # run container first - visit - http://localhost:6333/dashboard#/collections
+    # prefer_grpc=True,
+    collection_name="pushpak_rag_health_documents_1",
+)
+
+print(f"indexing of documents done....")
+
